ea0ecc226013a9523c330ca3830988d8f0c884f3
ShawnPark <tomatosale7774@gmail.com>
2021-02-19 00:42:50 +0900

    Initial Commit

---
+++ Jinstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jinstagram
+//
+//  Created by 박진서 on 2021/02/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
